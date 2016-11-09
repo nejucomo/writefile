2c000c0073c3a85c0cc225ef480c036a7fa32f68
Nathan Wilcox <nejucomo@gmail.com>
2016-11-09 09:29:37 -0800

    Untested first draft implementation.

--- writefile/main.py
+++ writefile/main.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python
 
+import os
 import sys
+import errno
 import argparse
+
+
+BUFSIZE = 2**20
 
 
 def main(args=sys.argv[1:]):
@@ -9,12 +14,31 @@ def main(args=sys.argv[1:]):
     Write stdin to a given path, creating directories as necessary.
     """
     opts = parse_args(args)
-    raise NotImplementedError('main')
+    ensure_dir_exists(os.path.basename(opts.PATH))
+    with file(opts.PATH, 'w') as f:
+        buf = sys.stdin.read(BUFSIZE)
+        while buf:
+            f.write(buf)
+            buf = sys.stdin.read(BUFSIZE)
 
 
 def parse_args(args):
     p = argparse.ArgumentParser(description=main.__doc__)
+
+    p.add_argument(
+        'PATH',
+        help='The PATH to write.',
+    )
+
     return p.parse_args(args)
+
+
+def ensure_dir_exists(path):
+    try:
+        os.makedirs(path)
+    except os.error as e:
+        if e.errno != errno.EEXIST:
+            raise
 
 
 if __name__ == '__main__':
